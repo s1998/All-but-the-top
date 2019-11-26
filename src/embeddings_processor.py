bd9dcb611da5cc843c8042af99e4f5458e6e23d1
Sudhanshu Ranjan <s1998@users.noreply.github.com>
2019-11-26 20:35:56 +0530

    Update embeddings_processor.py

--- src/embeddings_processor.py
+++ src/embeddings_processor.py
@@ -1,6 +1,9 @@
 from sklearn.decomposition import PCA
 import copy
 
+# Fix as pointed out from here
+# https://gist.github.com/lgalke/febaaa1313d9c11f3bc8240defed8390#gistcomment-3084023
+
 def get_processed_embeddings(embedding_matrix_orig, n_components = 1):
 	pca = PCA(n_components=n_components)
 	embedding_matrix = copy.deepcopy(embedding_matrix_orig)
