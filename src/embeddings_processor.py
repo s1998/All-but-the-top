3f0b57c3ef2be740f3b25a072e40808aa8cca50b
Sudhanshu Ranjan <s1998@users.noreply.github.com>
2019-11-26 20:34:23 +0530

    Update embeddings_processor.py

--- src/embeddings_processor.py
+++ src/embeddings_processor.py
@@ -4,9 +4,10 @@ import copy
 def get_processed_embeddings(embedding_matrix_orig, n_components = 1):
 	pca = PCA(n_components=n_components)
 	embedding_matrix = copy.deepcopy(embedding_matrix_orig)
-	temp = embedding_matrix - np.average(embedding_matrix, axis=0)
+	mean = np.average(embedding_matrix, axis=0)
+	temp = embedding_matrix - mean
 	principalComponents = pca.fit_transform(temp)
 	principalAxes = pca.components_
 	toSubstract = np.matmul(np.matmul(embedding_matrix, principalAxes.T), principalAxes)
-	processed = embedding_matrix - toSubstract
+	processed = mean - toSubstract
 	return processed
